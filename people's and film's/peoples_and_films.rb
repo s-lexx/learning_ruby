b80975daf5cfa63987cd58a5f1797df946cd3ae4
s-lexx <s-lexx30@yandex.ru>
2021-09-12 21:32:45 +0300

    Refactoring

--- people's and film's/peoples_and_films.rb
+++ people's and film's/peoples_and_films.rb
@@ -17,11 +17,12 @@ man1 = Human.new(gets.encode("UTF-8"),nil,20)
 
 puts "Введите имя второго человека"
 
-man2 = Human.new(gets.encode("UTF-8"), nil, 25)
+man2 = Human.new(gets.encode("UTF-8"), nil,25)
 
 puts "Введите имя трерьего человека"
 
-man3 = Human.new(gets.encode("UTF-8"), nil, 30)
+man3 = Human.new(gets.encode("UTF-8"), nil,30)
+
 
 puts "Любимый фильм 1-го человека" # Запрашиваем любимые фильмы для ранее введенных людей
 
@@ -35,8 +36,10 @@ puts "Любимый фильм 3-го человека"
 
 man3.set_film(gets.encode("UTF-8"))
 
+
 puts "#{man1.full_name}с любимым фильмом: #{man1.get_film}" # Выводим людей и их любимые фильмы
 
 puts "#{man2.full_name}с любимым фильмом: #{man2.get_film}"
 
 puts "#{man3.full_name}с любимым фильмом: #{man3.get_film}"
+
