383e2bd46ac779ba899ff9acb1ca9927f6fdb1d0
s-lexx <s-lexx30@yandex.ru>
2021-09-12 21:30:17 +0300

    Rafactoring

--- people's and film's/Film.rb
+++ people's and film's/Film.rb
@@ -1,19 +1,19 @@
 class Film
 
-  def initialize(film, drejtor) 
+  def initialize(film, drejtor) # конструктор класса и определения переменных
 
     @film_name = film
     @drejtor_name = drejtor
 
   end
 
-  def film_name
+  def film_name # метод для получения названия фильма
 
     return @film_name
 
   end
 
-  def drejtor_name
+  def drejtor_name # метод для получения имени режиссера
 
     return @drejtor_name
 
--- people's and film's/Human.rb
+++ people's and film's/Human.rb
@@ -1,6 +1,6 @@
 class Human
 
-  def initialize(first_mane, middle_name, age)
+  def initialize(first_mane, middle_name, age) # конструктор класса, определение необходимых переменных
 
     @first_name = first_mane
     @middle_name = middle_name
@@ -9,25 +9,25 @@ class Human
 
   end
 
-  def old?
+  def old? # метод для определения является человек пожилой или нет
 
     @age > 60 ? true : false 
 
   end
 
-  def set_film(favorite_movie)
+  def set_film(favorite_movie) # назначения любимого фильма для человека 
 
     @favorite_movie = favorite_movie
 
   end
 
-  def get_film
+  def get_film # получение названия любимого фильма для человека
 
     return @favorite_movie
 
   end
   
-  def full_name
+  def full_name # вывод полного имени с учетом признака пожилой или не пожилой человек
 
     old? ? "#{@first_name} #{@middle_name}" : @first_name
 
