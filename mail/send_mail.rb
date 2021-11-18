b02bfe29b7949115ce1907486b210d157304181c
s-lexx <s-lexx30@yandex.ru>
2021-11-18 21:41:03 +0300

    refactoring

--- mail/send_mail.rb
+++ mail/send_mail.rb
@@ -1,22 +1,39 @@
 require "pony"
 require "io/console"
 
+if (Gem.win_platform?)  ## Блок кода для устранения проблемы c кириллицей на платформе Windows
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+
+
 my_mail = "alexey83@list.ru"  #сохраняем почту отправителя]
 
 puts "Введите пароль от вашеней почты #{my_mail} для отправки письма:" # запрашиваем пароль для почты
+
 password = STDIN.noecho(&:gets).chomp
 
 
 puts " Введите адресата письма:" # запрашиваем адресата для пиьсма и текст письма
+
 mail_to = STDIN.gets.chomp
 
+puts " Введите тему для письма"
+
+subject = STDIN.gets.encode('UTF-8').chomp
+
 puts "Что написать в письме"
-body = STDIN.gets.chomp
+
+body = STDIN.gets.encode('UTF-8').chomp
 
 Pony.mail(
 {
 
-  :subject => "Привет из руби!",
+  :subject => subject,
   :body => body,
   :to => mail_to,
   :from => my_mail,
